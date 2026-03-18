2c6bb6275b82c19057b65acedde15775b62ff172
Brandon Wu <brandon.wu@sifive.com>
2026-03-18 01:06:56 -0700

    fixup! autogen

--- auto-generated/gnu-api-tests/vfwbdota.c
+++ auto-generated/gnu-api-tests/vfwbdota.c
@@ -34,36 +34,4 @@ vfloat32m4_t test_vfwbdota_vv_bf16m1_f32m4_m(vbool8_t vm, vfloat32m4_t vd, vbflo
 vfloat32m8_t test_vfwbdota_vv_bf16m1_f32m8_m(vbool4_t vm, vfloat32m8_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
   return __riscv_vfwbdota_vv_bf16m1_f32m8_m(vm, vd, vs2, vs1, 0, vl);
 }
-
-vfloat32m1_t test_vfwbdota_vv_bf16m1_f32m1_rm(vfloat32m1_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m1_rm(vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m2_t test_vfwbdota_vv_bf16m1_f32m2_rm(vfloat32m2_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m2_rm(vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m4_t test_vfwbdota_vv_bf16m1_f32m4_rm(vfloat32m4_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m4_rm(vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m8_t test_vfwbdota_vv_bf16m1_f32m8_rm(vfloat32m8_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m8_rm(vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m1_t test_vfwbdota_vv_bf16m1_f32m1_rm_m(vbool32_t vm, vfloat32m1_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m1_rm_m(vm, vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m2_t test_vfwbdota_vv_bf16m1_f32m2_rm_m(vbool16_t vm, vfloat32m2_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m2_rm_m(vm, vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m4_t test_vfwbdota_vv_bf16m1_f32m4_rm_m(vbool8_t vm, vfloat32m4_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m4_rm_m(vm, vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-
-vfloat32m8_t test_vfwbdota_vv_bf16m1_f32m8_rm_m(vbool4_t vm, vfloat32m8_t vd, vbfloat16m8_t vs2, vbfloat16m1_t vs1, size_t vl) {
-  return __riscv_vfwbdota_vv_bf16m1_f32m8_rm_m(vm, vd, vs2, vs1, 0, __RISCV_FRM_RNE, vl);
-}
-/* { dg-final { scan-assembler-times {vseti?vli\s+[a-z0-9]+,\s*[a-z0-9]+,\s*e[0-9]+,\s*mf?[1248],\s*t[au],\s*m[au]\s+vfwbdota\.[ivxfswum.]+\s+} 16 } } */
+/* { dg-final { scan-assembler-times {vseti?vli\s+[a-z0-9]+,\s*[a-z0-9]+,\s*e[0-9]+,\s*mf?[1248],\s*t[au],\s*m[au]\s+vfwbdota\.[ivxfswum.]+\s+} 8 } } */
