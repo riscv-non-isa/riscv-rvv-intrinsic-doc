f72e0fd805f39175483fe048d9bcd016c5b2a328
Pengcheng Wang <wangpengcheng.pp@bytedance.com>
2026-06-18 13:19:43 +0800

    [Auto-gen] Update bfloat16 tests under ../auto-generated. (make git-commit-autogen-bf16-test)

---
+++ auto-generated/bfloat16/llvm-overloaded-tests/vunzipe.c
@@ -0,0 +1,54 @@
+// REQUIRES: riscv-registered-target
+// RUN: %clang_cc1 -triple riscv64 -disable-O0-optnone \
+// RUN:   -target-feature +zvfbfmin \
+// RUN:   -target-feature +zvzip \
+// RUN:   -target-feature +experimental \
+// RUN:   -emit-llvm %s -o - | opt -S -passes=mem2reg | \
+// RUN:   FileCheck --check-prefix=CHECK-RV64 %s
+
+#include <riscv_vector.h>
+
+vbfloat16mf4_t test_vunzipe_v_bf16mf4(vbfloat16mf2_t vs, size_t vl) {
+  return __riscv_vunzipe(vs, vl);
+}
+
+vbfloat16mf2_t test_vunzipe_v_bf16mf2(vbfloat16m1_t vs, size_t vl) {
+  return __riscv_vunzipe(vs, vl);
+}
+
+vbfloat16m1_t test_vunzipe_v_bf16m1(vbfloat16m2_t vs, size_t vl) {
+  return __riscv_vunzipe(vs, vl);
+}
+
+vbfloat16m2_t test_vunzipe_v_bf16m2(vbfloat16m4_t vs, size_t vl) {
+  return __riscv_vunzipe(vs, vl);
+}
+
+vbfloat16m4_t test_vunzipe_v_bf16m4(vbfloat16m8_t vs, size_t vl) {
+  return __riscv_vunzipe(vs, vl);
+}
+
+vbfloat16mf4_t test_vunzipe_v_bf16mf4_m(vbool64_t vm, vbfloat16mf2_t vs,
+                                        size_t vl) {
+  return __riscv_vunzipe(vm, vs, vl);
+}
+
+vbfloat16mf2_t test_vunzipe_v_bf16mf2_m(vbool32_t vm, vbfloat16m1_t vs,
+                                        size_t vl) {
+  return __riscv_vunzipe(vm, vs, vl);
+}
+
+vbfloat16m1_t test_vunzipe_v_bf16m1_m(vbool16_t vm, vbfloat16m2_t vs,
+                                      size_t vl) {
+  return __riscv_vunzipe(vm, vs, vl);
+}
+
+vbfloat16m2_t test_vunzipe_v_bf16m2_m(vbool8_t vm, vbfloat16m4_t vs,
+                                      size_t vl) {
+  return __riscv_vunzipe(vm, vs, vl);
+}
+
+vbfloat16m4_t test_vunzipe_v_bf16m4_m(vbool4_t vm, vbfloat16m8_t vs,
+                                      size_t vl) {
+  return __riscv_vunzipe(vm, vs, vl);
+}
